f9932b542a5b29f70ea3c4f1275ba5ec1c00179f
mennasalah <52379016+mennasala@users.noreply.github.com>
2021-07-24 16:02:59 +0200

    Add source code

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCyan;
+            this.button1.Font = new System.Drawing.Font("Tempus Sans ITC", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.Location = new System.Drawing.Point(174, 173);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(692, 127);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Program";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Menu;
+            this.listView1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(6, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(912, 665);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1254, 666);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1152, 554);
+            this.panel1.TabIndex = 10;
+            this.panel1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 666);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        // 1-single  2-multi Picture  3-slide Show  4-exit  
+        Button b1, b2, b3, b4, upload, Return;
+        public Form1()
+        {
+            upload = new Button();
+            upload.Size = new Size(220, 60);
+            upload.Text = "Upload";
+            Return = new Button();
+            Return.Size = new Size(220, 60);
+            Return.Text = "Return";
+            Return.Font = new Font(Return.Font.FontFamily, 25);
+            upload.Location = new Point(300, 80);
+            upload.Font = new Font(upload.Font.FontFamily, 25);
+            b1 = new Button();
+            b1.Size = new Size(220, 60);
+            b1.Text = "Single Picture";
+            b1.Location = new Point(300, 80);
+            b1.Font = new Font(b1.Font.FontFamily, 25);
+            b2 = new Button();
+            b2.Size = new Size(220, 60);
+            b2.Text = "Multi-Picture ";
+            b2.Location = new Point(300, 150);
+            b2.Font = new Font(b2.Font.FontFamily, 25);
+            b3 = new Button();
+            b3.Size = new Size(220, 60);
+            b3.Text = "slide show";
+            b3.Location = new Point(300, 220);
+            b3.Font = new Font(b3.Font.FontFamily, 25);
+            b4 = new Button();
+            b4.Size = new Size(220, 60);
+            b4.Text = "Exit";
+            b4.Location = new Point(300, 150);
+            b4.Font = new Font(b4.Font.FontFamily, 25);
+            InitializeComponent();
+        }
+        public Image imagebox
+        {
+            set
+            {
+                this.pictureBox1.Image = value;
+                this.pictureBox1.Size = value.Size;
+            }
+        }
+        List<string> picturName = new List<string>();
+        List<ListViewItem> list_of_pictures = new List<ListViewItem>();
+        private void call()
+        {
+            list_of_pictures.Clear();
+            foreach (var i in listView1.SelectedItems)
+            {
+                ListViewItem a = (ListViewItem)i;
+                list_of_pictures.Add(a);
+            }
+        }
+
+        private void upload_Click(object sender, EventArgs e) // upload
+        {
+            timer1.Interval = 1;
+            listView1.Visible = true;
+            using (OpenFileDialog Open = new OpenFileDialog()
+            {
+                Multiselect = true,
+                ValidateNames = true,
+            })
+            {
+                if (Open.ShowDialog() == DialogResult.OK)
+                {
+                    picturName.Clear();
+                    listView1.Items.Clear();
+                    foreach (string name in Open.FileNames)
+                    {
+                        FileInfo info = new FileInfo(name);
+                        picturName.Add(info.FullName);
+                        listView1.Items.Add(info.Name, 0);
+                    }
+                }
+            }
+            Controls.Add(b1);
+            Controls.Add(b2);
+            Controls.Add(b3);
+            Controls.Add(Return);
+            upload.Location = new Point(700, 50);
+            b1.Location = new Point(700, 120);
+            b2.Location = new Point(700, 190);
+            b3.Location = new Point(700, 260);
+            Return.Location = new Point(700, 340);
+            b4.Location = new Point(700, 410);
+            listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+        }
+        private void listView1_ItemActivate(object sender, EventArgs e)// when select an image it will be displayed
+        {
+            if (listView1.Focused != null)
+            {
+                Return.Visible = true;
+                Return.BringToFront();
+                pictureBox1.Visible = true;
+                listView1.Visible = false;
+                Image img = Image.FromFile(picturName[listView1.FocusedItem.Index]);
+                this.imagebox = img;
+                FileInfo fi = new FileInfo(picturName[listView1.FocusedItem.Index]);
+                this.Text = fi.Name;
+            }
+        }
+        private void Return_Click(object sender, EventArgs e)// Return
+        {
+            b1.Visible = true;
+            b2.Visible = true;
+            b3.Visible = true;
+            panel1.Visible = false;
+            pictureBox1.Visible = false;
+            listView1.Visible = true;
+            Return.Visible = false;
+            // if there is any picture last from the last run to the program 
+            foreach (Control i in panel1.Controls)
+                panel1.Controls.Remove(i);
+            timer1.Stop();
+            this.Text = "Pictures";
+        }
+        private void b1_Click(object sender, EventArgs e)// single picture
+        {
+            if (listView1.Focused != null)
+            {
+                Return.Visible = true;
+                Return.BringToFront();
+                pictureBox1.Visible = true;
+                listView1.Visible = false;
+                Image img = Image.FromFile(picturName[listView1.FocusedItem.Index]);
+                this.imagebox = img;
+                FileInfo fi = new FileInfo(picturName[listView1.FocusedItem.Index]);
+                this.Text = fi.Name;
+            }
+        }
+        private void b2_Click(object sender, EventArgs e)// multi pictur
+        {
+            call();
+            if (list_of_pictures.Count() > 1)
+            {
+                Return.Visible = true;
+                b4.Visible = true;
+                panel1.Visible = true;
+                listView1.Visible = false;
+                b1.Visible = false;
+                b2.Visible = false;
+                b3.Visible = false;
+                upload.Visible = false;
+                b4.BringToFront();
+                Return.BringToFront();
+                int x = 0, y = 0;
+                foreach (var img in list_of_pictures)
+                {
+                    PictureBox p1 = new PictureBox();
+                    p1.Image = Image.FromFile(picturName[img.Index]);
+                    p1.Location = new Point(x, y);
+                    p1.Size = new Size(200, 200);
+                    p1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    panel1.Controls.Add(p1);
+                    x += 200;
+                    if (x > panel1.Width)
+                    {
+                        x = 0; y += 204;
+                    }
+                }
+                this.Text = "Multi selection";
+            }
+
+        }
+        int idx = 0;
+        private void b3_Click(object sender, EventArgs e) // slide show
+        {
+            call();
+            if (list_of_pictures.Count() > 1)
+            {
+                b4.Visible = true;
+                Return.Visible = true;
+                b4.BringToFront();
+                Return.BringToFront();
+                listView1.Visible = false;
+                //show images
+                idx = 0;
+                timer1.Enabled = true;
+                timer1.Tick += new System.EventHandler(OnTimerEvent);
+            }
+        }
+        private void OnTimerEvent(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            if (list_of_pictures.Count() > 1)
+            {
+                pictureBox1.Visible = true;
+                Image img = Image.FromFile(picturName[list_of_pictures[idx].Index]);
+                pictureBox1.Image = img;
+                FileInfo fi = new FileInfo(picturName[list_of_pictures[idx].Index]);
+                Text = fi.Name;
+                idx++;
+                if (idx == list_of_pictures.Count()) idx = 0;
+            }
+        }
+        private void b4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "Pictures";
+            Controls.Remove(button1);
+            button1.Dispose();
+            Controls.Add(upload);
+            Controls.Add(b4);
+            b1.Click += b1_Click;
+            b2.Click += b2_Click;
+            b3.Click += b3_Click;
+            b4.Click += b4_Click;
+            Return.Click += Return_Click;
+            upload.Click += upload_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
